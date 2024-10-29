c79384ef53f378f1f202d66fb59cc377116f6a58
ashish <ashishmangla1991@gmail.com>
2024-10-29 12:01:51 +0530

    add stack navigation

--- navigation/MainNavigation.tsx
+++ navigation/MainNavigation.tsx
@@ -5,11 +5,16 @@ import Profile from "../screens/Profile/Profile";
 
 const Stack = createStackNavigator();
 
-const MainNavigation = () =>{
-    return <Stack.Navigator screenOptions={{header:() => null,headerShown:false}} initialRouteName={Routes.Home}>
-        <Stack.Screen name={Routes.Home}  component={Home}/>
-        <Stack.Screen name={Routes.Profile}  component={Profile}/>
+const MainNavigation = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{ header: () => null, headerShown: false }}
+      initialRouteName={Routes.Home}
+    >
+      <Stack.Screen name={Routes.Home} component={Home} />
+      <Stack.Screen name={Routes.Profile} component={Profile} />
     </Stack.Navigator>
-}
+  );
+};
 
-export default MainNavigation
+export default MainNavigation;
