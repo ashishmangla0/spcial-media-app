0d92a7b512d235f0f1a647739aab8c63d629a183
ashish <ashishmangla1991@gmail.com>
2024-11-01 20:40:25 +0530

    add new code

--- navigation/MainNavigation.tsx
+++ navigation/MainNavigation.tsx
@@ -2,8 +2,22 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { Routes } from "./Routes";
 import Home from "../screens/Home/Home";
 import Profile from "../screens/Profile/Profile";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const MainMenuNavigation = () =>{
+  return(
+    <Drawer.Navigator 
+    screenOptions={{ header: () => null, headerShown: false }}
+    initialRouteName={Routes.Home}>
+      <Drawer.Screen name={Routes.Home} component={Home}></Drawer.Screen>
+      <Stack.Screen name={Routes.Profile} component={Profile} />
+    
+    </Drawer.Navigator>
+  )
+}
 
 const MainNavigation = () => {
   return (
@@ -11,8 +25,10 @@ const MainNavigation = () => {
       screenOptions={{ header: () => null, headerShown: false }}
       initialRouteName={Routes.Home}
     >
-      <Stack.Screen name={Routes.Home} component={Home} />
+       <Stack.Screen name={'Drawer'} component={MainMenuNavigation} />
+      {/* <Stack.Screen name={Routes.Home} component={Home} /> */}
       <Stack.Screen name={Routes.Profile} component={Profile} />
+     
     </Stack.Navigator>
   );
 };
